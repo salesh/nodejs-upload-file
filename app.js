8e2f120df4deab2c6497bd13c2f1451c9431f818
salesh <sasa.cvetkovic@yahoo.com>
2020-05-05 22:19:16 +0200

    UUI

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser= require('body-parser')
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
 
 const indexRouter = require('./routes/index');
 
